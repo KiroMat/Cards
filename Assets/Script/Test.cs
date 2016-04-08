d8a9971a570907d15d05b6bb5d3301f85552042b
Mateusz Witkowski <mattomwit@gmail.com>
2016-04-08 21:39:21 +0200

    commit2 test

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -5,8 +5,10 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        Debug.Log("Dodałem, co dalej?");
+        Debug.Log("Pozdrów Asie!!!!");
+    }
 	
 	// Update is called once per frame
 	void Update () {
