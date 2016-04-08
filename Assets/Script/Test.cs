54c0cf02581544adc66a18ca2e21dff3ee028d71
Dzakub <kirodzakub@gmail.com>
2016-04-08 21:25:46 +0200

    Testowy Commit

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -5,8 +5,13 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Debug.Log("Tworzenie Gry");
+        Debug.Log("gggggggggg");
+        Debug.Log("mmmmmmmm");
+        Debug.Log("ffffff");
+        Debug.Log("qqqqqqqqqqqqq");
+        Debug.Log("mmmmmmmmm");
+    }
 	
 	// Update is called once per frame
 	void Update () {
