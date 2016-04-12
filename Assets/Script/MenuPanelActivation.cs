05bce18ff9065e763d081be6537e1153c6a708e0
Mateusz Witkowski <mattomwit@gmail.com>
2016-04-12 17:35:08 +0200

    Scene Menu edited, Added Animation, panel, buttons, MenuPanelActivation script.

---
+++ Assets/Script/MenuPanelActivation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuPanelActivation : MonoBehaviour {
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnMouseEnter()
+    {
+        anim.SetTrigger("ActivatePanel");
+    }
+    void OnMouseExit()
+    {
+        anim.SetTrigger("DeactivatePanel");
+    }
+
+    //this will change later on to lunch scene given by string
+    public void LunchSceneMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
