3c3f16f5f1b3e58ce4abbbc4a5c4fb08fed1612e
Dzakub <kirodzakub@gmail.com>
2016-04-16 09:44:53 +0200

    Create files for prsentation

---
+++ Assets/Presentation/Carddd.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Carddd : MonoBehaviour, IMoveHandler, IBeginDragHandler, IEndDragHandler
+{
+
+    public void OnMove(AxisEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        var ooo = eventData.pointerEnter.GetComponent<Slot>();
+        if (ooo != null)
+        {
+            //ooo.cart = this;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Presentation/Slot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public delegate void ChangedEventHandler(Carddd name);
+
+[Serializable]
+public class Slot : MonoBehaviour  {
+
+    public event ChangedEventHandler Changed;
+  
+    public int Amount;
+
+    private Carddd card;
+
+    public Carddd Card
+    {
+        set
+        {
+            if (card != null)
+                return;
+
+            card = value;
+            Changed(card);
+        }
+    }
+
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
