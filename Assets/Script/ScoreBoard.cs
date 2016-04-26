e024ce6c39a6e0fa0c4f455a2e4fba558f55d77b
Mateusz Witkowski <mattomwit@gmail.com>
2016-04-26 15:58:55 +0200

    Modified ScoreBoard prefab and script

--- Assets/Script/ScoreBoard.cs
+++ Assets/Script/ScoreBoard.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Internal;
 
 public class ScoreBoard : MonoBehaviour {
     public GameObject activePlayerParticle;
@@ -9,7 +10,7 @@ public class ScoreBoard : MonoBehaviour {
     public GameObject sliderLeft;
     public GameObject sliderRight;
     public GameObject sliderGameObject;
-
+    Vector3 tempVector;
     public float sliderValue
     {
         get { return sliderGameObject.GetComponent<Slider>().value; }
@@ -40,6 +41,8 @@ public class ScoreBoard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    //    activePlayerParticle.transform.position = 
+
+        tempVector = UnityEngine.Camera.main.ScreenToWorldPoint(shieldLeft.transform.localPosition);
+        activePlayerParticle.transform.position = new Vector3(tempVector.x, tempVector.y, 0);
 	}
 }
